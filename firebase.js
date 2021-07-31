80a57e89a6fcadceda0b2a5f0d3e3d029e10060e
Nazariy Dumanskyy <nazariy1995@gmail.com>
2021-07-30 22:43:46 -0700

    Day 3 of the challenge

---
+++ firebase.js
@@ -0,0 +1,13 @@
+var firebaseConfig = {
+apiKey: "AIzaSyCoifsQZU4X_ARBbMGEiIQSjlk5g3qpUuU",
+authDomain: "clone-two-8c1f5.firebaseapp.com",
+projectId: "clone-two-8c1f5",
+storageBucket: "clone-two-8c1f5.appspot.com",
+messagingSenderId: "793216905054",
+appId: "1:793216905054:web:a652681c4d0ae55c3069cf",
+measurementId: "G-LC641H0GZV"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
+
